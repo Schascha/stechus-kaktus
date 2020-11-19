a4a3a2b71223ad5a3c5089952d4d85a353213090
Schascha <contact.schascha@gmail.com>
2020-11-19 21:10:26 +0100

    PWA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import './registerServiceWorker';
 
 new Vue({
 	render: h => h(App)
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
 		? '/stechus-kaktus/'
-		: '/'
- }
+		: '/',
+	pwa: {
+		themeColor: '#C19A5B',
+		iconPaths: {
+			favicon32: 'img/icons/favicon-32x32.png',
+			favicon16: 'img/icons/favicon-16x16.png',
+			appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+			maskIcon: 'img/icons/safari-pinned-tab.svg',
+			msTileImage: 'img/icons/msapplication-icon-144x144.png'
+		}
+	}
+}
