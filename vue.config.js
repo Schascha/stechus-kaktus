66bf64d340a2c2e5557626796bdffbe48b47204a
Schascha <contact.schascha@gmail.com>
2020-11-19 22:46:05 +0100

    Add missing icons

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,10 @@ module.exports = {
 		? '/stechus-kaktus/'
 		: '/',
 	pwa: {
+		name: 'Stechus Kaktus – Romanizer',
 		themeColor: '#C19A5B',
-		iconPaths: {
-			favicon32: 'img/icons/favicon-32x32.png',
-			favicon16: 'img/icons/favicon-16x16.png',
-			appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-			maskIcon: 'img/icons/safari-pinned-tab.svg',
-			msTileImage: 'img/icons/msapplication-icon-144x144.png'
+		manifestOptions: {
+			icons: []
 		}
 	}
 }
