08144c52a5b6173338a63b4e657247ec396a6b8f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-20 21:40:09 +0000

    Deploy Schascha/stechus-kaktus to github.com/Schascha/stechus-kaktus.git:gh-pages

---
+++ precache-manifest.87d90c7ab859c6bcee6b720ff7209d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66895dd7a67dfa07ce5cad5e891b3632",
+    "url": "/stechus-kaktus/.htaccess"
+  },
+  {
+    "revision": "835c9b4ea247c9645a7b",
+    "url": "/stechus-kaktus/css/app.67feb39a.css"
+  },
+  {
+    "revision": "cbbd13da77a0cc1e10f3096e20d1050b",
+    "url": "/stechus-kaktus/index.html"
+  },
+  {
+    "revision": "835c9b4ea247c9645a7b",
+    "url": "/stechus-kaktus/js/app.6cc8d833.js"
+  },
+  {
+    "revision": "3c844a26845d830935f8",
+    "url": "/stechus-kaktus/js/chunk-vendors.68827d26.js"
+  },
+  {
+    "revision": "d8ee3a728547e75dd8b72cf27d874abf",
+    "url": "/stechus-kaktus/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/stechus-kaktus/robots.txt"
+  }
+]);
