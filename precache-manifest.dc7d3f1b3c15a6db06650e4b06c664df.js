baa4f3f06e6931ca9f86f11c206f29724772fd0f
Schascha <contact.schascha@gmail.com>
2020-11-29 20:28:39 +0000

    Deploying to gh-pages from  @ 892f271ca7a3be6cae15bac408766b83e8c183cc 🚀

--- precache-manifest.dc7d3f1b3c15a6db06650e4b06c664df.js
+++ precache-manifest.dc7d3f1b3c15a6db06650e4b06c664df.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8656dcb10b4efca338d6",
-    "url": "/stechus-kaktus/css/app.1ac9568b.css"
+    "revision": "d690828eba174afa5cb8",
+    "url": "/stechus-kaktus/css/app.b2b15fe0.css"
   },
   {
-    "revision": "a5f348ea7caee6d353d7c8d54ecf17b5",
+    "revision": "36891a799a0023793f0df83a6a5a58dd",
     "url": "/stechus-kaktus/index.html"
   },
   {
-    "revision": "8656dcb10b4efca338d6",
-    "url": "/stechus-kaktus/js/app.3966af60.js"
+    "revision": "d690828eba174afa5cb8",
+    "url": "/stechus-kaktus/js/app.336238ca.js"
   },
   {
     "revision": "f898a0c17c42e60fc2f0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stechus-kaktus/precache-manifest.03c0afa467baa63a52dc67c164cc3419.js"
+  "/stechus-kaktus/precache-manifest.dc7d3f1b3c15a6db06650e4b06c664df.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "stechus-kaktus"});
